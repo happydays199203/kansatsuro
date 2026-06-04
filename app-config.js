3297921ee66034289b1768d7465799690166a32a
happydays199203 <happydays199203@gmail.com>
2026-06-04 19:34:55 +0900

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,6 +1,6 @@
 // Supabaseを使う場合は、app-config.example.js を参考にこの値を変更してください。
 window.KANSATSURO_CONFIG = {
-  SUPABASE_URL: "https://edyiywebbzssmaspeehi.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://edyiywebbzssmaspeehi.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_EeBKRIvhTCJGf9tYAikAaw_kcelxCZO",
   STORAGE_BUCKET: "observation-photos",
   DEFAULT_WORKSPACE_NAME: "観察チーム",
